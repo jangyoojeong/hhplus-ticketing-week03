43f1413fc245652da22df5ee6469ab1c9218152f
JangYooJeong <gldjfh59590@gmail.com>
2024-07-03 04:11:25 +0900

    Initial commit

---
+++ src/test/java/org/hhplus/ticketing/TicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hhplus.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
