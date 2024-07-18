3b33c17347c112a1a92f40ef7d0aedf1697d0fb2
JangYooJeong <gldjfh59590@gmail.com>
2024-07-19 06:31:47 +0900

    Scheduler 개발

---
+++ src/main/java/org/hhplus/ticketing/interfaces/scheduler/QueueScheduler.java
@@ -0,0 +1,25 @@
+package org.hhplus.ticketing.interfaces.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.hhplus.ticketing.application.queue.facade.QueueFacade;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class QueueScheduler {
+
+    private final QueueFacade queueFacade;
+
+    /**
+     * 대기열 상태를 업데이트합니다.
+     * 1. 만료 대상 토큰 만료
+     * 2. 빈자리 만큼 활성화
+     * >> 스케줄러 2분 주기 작업 (@Scheduled(fixedRate = 2 * 60 * 1000))
+     */
+    @Scheduled(fixedRate = 2 * 60 * 1000)
+    public void updateQueueStatuses() {
+        queueFacade.updateQueueStatuses();
+    }
+
+}
