a4aab62e9e0a2db23b8c7a96d20df3de67d22c3b
JangYooJeong <gldjfh59590@gmail.com>
2024-08-02 07:44:48 +0900

    레디스 관련 설정

--- src/main/java/org/hhplus/ticketing/TicketingApplication.java
+++ src/main/java/org/hhplus/ticketing/TicketingApplication.java
@@ -2,11 +2,13 @@ package org.hhplus.ticketing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 @EnableAspectJAutoProxy
 public class TicketingApplication {
 
