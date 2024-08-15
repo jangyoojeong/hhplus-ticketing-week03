c2fd5fb020be7b93363b7110c8e3b7e6cd5f0e38
JangYooJeong <gldjfh59590@gmail.com>
2024-08-16 07:37:20 +0900

    카프카 로직 테스트 (consumer, producer, 스케줄러)

---
+++ src/test/java/org/hhplus/ticketing/domain/common/messaging/MessageSenderTest.java
@@ -0,0 +1,66 @@
+package org.hhplus.ticketing.domain.common.messaging;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.SettableListenableFuture;
+
+import java.util.concurrent.CompletableFuture;
+
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+
+public class MessageSenderTest {
+
+    @Mock
+    private MessageProducer messageProducer;
+
+    @InjectMocks
+    private MessageSender messageSender;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("🟢 메시지_전송_테스트_메시지_전송에_성공한다")
+    void sendMessage_메시지_전송_테스트_메시지_전송에_성공한다() {
+        // Given
+        SettableListenableFuture<SendResult<String, String>> future = new SettableListenableFuture<>();
+        future.set(mock(SendResult.class));
+        given(messageProducer.send(anyString(), anyString(), anyString())).willReturn(future.completable());
+
+        // When
+        CompletableFuture<Boolean> result = messageSender.sendMessage("eventType", "messageKey", "message");
+
+        // Then
+        assertTrue(result.join()); // 메시지 전송 성공
+        verify(messageProducer, times(1)).send(anyString(), anyString(), anyString());
+    }
+
+    @Test
+    @DisplayName("🔴 메시지_전송_테스트_메시지_전송에_실패한다")
+    void sendMessage_메시지_전송_테스트_메시지_전송에_실패한다다() {
+        // Given
+        SettableListenableFuture<SendResult<String, String>> future = new SettableListenableFuture<>();
+        future.setException(new RuntimeException("Kafka send failure"));
+        given(messageProducer.send(anyString(), anyString(), anyString())).willReturn(future.completable());
+
+        // When
+        CompletableFuture<Boolean> result = messageSender.sendMessage("eventType", "messageKey", "message");
+
+        // Then
+        assertFalse(result.join()); // 메시지 전송 실패
+        verify(messageProducer, times(1)).send(anyString(), anyString(), anyString());
+    }
+}
---
+++ src/test/java/org/hhplus/ticketing/domain/outbox/OutboxServiceTest.java
@@ -0,0 +1,187 @@
+package org.hhplus.ticketing.domain.outbox;
+
+import org.hhplus.ticketing.domain.common.exception.CustomException;
+import org.hhplus.ticketing.domain.common.exception.ErrorCode;
+import org.hhplus.ticketing.domain.common.messaging.MessageSender;
+import org.hhplus.ticketing.domain.outbox.model.Outbox;
+import org.hhplus.ticketing.domain.outbox.model.OutboxCommand;
+import org.hhplus.ticketing.domain.outbox.model.constants.OutboxConstants;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+// 아웃박스 서비스 단위테스트입니다.
+public class OutboxServiceTest {
+
+    @InjectMocks
+    private OutboxService outboxService;
+
+    @Mock
+    private OutboxRepository outboxRepository;
+
+    @Mock
+    private MessageSender messageSender;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("🟢 아웃박스_저장_테스트_저장_후_저장된_정보가_리턴된다")
+    void saveTest_아웃박스_저장_테스트_저장_후_저장된_정보가_리턴된다() {
+        // Given
+        OutboxCommand.save command = new OutboxCommand.save("1", "PAYMENT", "PAYMENT_SUCCESS", "MESSAGE");
+        Outbox expectedOutbox = Outbox.from(command);
+
+        given(outboxRepository.save(any(Outbox.class))).willReturn(expectedOutbox);
+
+        // When
+        Outbox result = outboxService.save(command);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(expectedOutbox, result);
+        verify(outboxRepository, times(1)).save(any(Outbox.class));
+    }
+
+    @Test
+    @DisplayName("🟢 아웃박스_상태변경_테스트_발행상태로_변경된다")
+    void updateSentTest_아웃박스_상태변경_테스트_발행상태로_변경된다() {
+        // Given
+        OutboxCommand.updateSent command = new OutboxCommand.updateSent("1", "PAYMENT", "PAYMENT_SUCCESS");
+        Outbox outbox = Outbox.builder()
+                .outboxId(1L)
+                .messageKey("1")
+                .domainType("PAYMENT")
+                .eventType("PAYMENT_SUCCESS")
+                .message("MESSAGE")
+                .isSent(false)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        given(outboxRepository.getOutbox(anyString(), anyString(), anyString())).willReturn(Optional.of(outbox));
+        given(outboxRepository.save(any(Outbox.class))).willReturn(outbox);
+
+        // When
+        Outbox result = outboxService.updateSent(command);
+
+        // Then
+        assertNotNull(result);
+        assertTrue(result.isSent());
+        assertNotNull(result.getSentAt());
+        verify(outboxRepository, times(1)).getOutbox(anyString(), anyString(), anyString());
+        verify(outboxRepository, times(1)).save(any(Outbox.class));
+    }
+
+    @Test
+    @DisplayName("🔴 아웃박스_상태변경_통합_테스트_이미_발행된_상태일_경우_INVALID_STATE_예외반환")
+    public void updateSentTest_아웃박스_상태변경_통합_테스트_이미_발행된_상태일_경우_INVALID_STATE_예외반환() {
+        // Given
+        OutboxCommand.updateSent command = new OutboxCommand.updateSent("1", "PAYMENT", "PAYMENT_SUCCESS");
+        Outbox outbox = Outbox.builder()
+                .outboxId(1L)
+                .messageKey("1")
+                .domainType("PAYMENT")
+                .eventType("PAYMENT_SUCCESS")
+                .message("MESSAGE")
+                .isSent(true)
+                .sentAt(LocalDateTime.now())
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        given(outboxRepository.getOutbox(anyString(), anyString(), anyString())).willReturn(Optional.ofNullable(outbox));
+
+        // Then
+        assertThatThrownBy(() -> outboxService.updateSent(command))
+                .isInstanceOf(CustomException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.INVALID_STATE);
+    }
+
+    @Test
+    @DisplayName("🔴 아웃박스_상태변경_테스트_조회된_아웃박스_데이터가_없을경우_OUTBOX_NOT_FOUND_예외반환")
+    public void updateSentTest_아웃박스_상태변경_테스트_조회된_아웃박스_데이터가_없을경우_OUTBOX_NOT_FOUND_예외반환() {
+        // Given
+        OutboxCommand.updateSent command = new OutboxCommand.updateSent("1", "PAYMENT", "PAYMENT_SUCCESS");
+
+        given(outboxRepository.getOutbox(anyString(), anyString(), anyString())).willReturn(Optional.empty());
+
+        // Then
+        assertThatThrownBy(() -> outboxService.updateSent(command))
+                .isInstanceOf(CustomException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.OUTBOX_NOT_FOUND);
+    }
+
+    @Test
+    @DisplayName("🟢 메시지_재발송_테스트_실패한_메시지를_재발송하고_발송상태로_변경된다")
+    void retryFailedMessages_메시지_재발송_테스트_실패한_메시지를_재발송하고_발송상태로_변경된다() {
+        // Given
+        Outbox outbox = Outbox.builder()
+                .outboxId(1L)
+                .messageKey("1")
+                .domainType("PAYMENT")
+                .eventType("PAYMENT_SUCCESS")
+                .message("MESSAGE")
+                .isSent(false)
+                .createdAt(LocalDateTime.now().minusMinutes(OutboxConstants.OUTBOX_RETRY_THRESHOLD_MINUTES + 1))
+                .build();
+
+        given(outboxRepository.getRetryTargetList(any(LocalDateTime.class))).willReturn(Collections.singletonList(outbox));
+        given(messageSender.sendMessage(anyString(), anyString(), anyString())).willReturn(CompletableFuture.completedFuture(true));
+        given(outboxRepository.save(any(Outbox.class))).willReturn(outbox);
+
+        // When
+        outboxService.retryFailedMessages();
+
+        // Then
+        verify(outboxRepository, times(1)).getRetryTargetList(any(LocalDateTime.class));
+        verify(messageSender, times(1)).sendMessage(anyString(), anyString(), anyString());
+        verify(outboxRepository, times(1)).save(any(Outbox.class));
+        assertTrue(outbox.isSent());
+        assertNotNull(outbox.getSentAt());
+    }
+
+    @Test
+    @DisplayName("🔴 메시지_재발송_테스트_메시지_발송에_실패한_경우_발송상태로_변경되지_않는다")
+    void retryFailedMessages_메시지_재발송_테스트_메시지_발송에_실패한_경우_발송상태로_변경되지_않는다() {
+        // Given
+        Outbox existingOutbox = Outbox.builder()
+                .outboxId(1L)
+                .messageKey("1")
+                .domainType("PAYMENT")
+                .eventType("PAYMENT_SUCCESS")
+                .message("MESSAGE")
+                .isSent(false)
+                .createdAt(LocalDateTime.now().minusMinutes(OutboxConstants.OUTBOX_RETRY_THRESHOLD_MINUTES + 1))
+                .build();
+
+        given(outboxRepository.getRetryTargetList(any(LocalDateTime.class))).willReturn(Collections.singletonList(existingOutbox));
+        given(messageSender.sendMessage(anyString(), anyString(), anyString())).willReturn(CompletableFuture.completedFuture(false));
+
+        // When
+        outboxService.retryFailedMessages();
+
+        // Then
+        verify(outboxRepository, times(1)).getRetryTargetList(any(LocalDateTime.class));
+        verify(messageSender, times(1)).sendMessage(anyString(), anyString(), anyString());
+        verify(outboxRepository, never()).save(existingOutbox); // 메시지 전송이 실패했으므로 save 메서드 호출 x
+        assertFalse(existingOutbox.isSent());
+        assertNull(existingOutbox.getSentAt());
+    }
+}
